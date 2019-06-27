74509ce1af56e3d81b3de3f836fbad9d2afa5ee0
xxlxep <xxlxep@gmail.com>
2019-06-27 22:00:43 +0300

    Different Type & Relations

--- schema/schema.js
+++ schema/schema.js
@@ -1,13 +1,41 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
+const movies = [
+  { id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1' },
+  { id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2' },
+  { id: '3', name: 'VVV', genre: 'Sci-Fi-Triller', directorId: '3' },
+  { id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4' },
+];
+
+const directors = [
+  { id: '1', name: 'Quentin Tarantino', age: 55 },
+  { id: '2', name: 'Michael Radford', age: 72 },
+  { id: '3', name: 'James McTeigue', age: 51 },
+  { id: '4', name: 'Gyu Ritcne', age: 50 },
+];
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    director: {
+      type: DirectorType,
+      resolve(parent, args) {
+        return directors.find(director => director.id === parent.id);
+      },
+    },
+  }),
+});
+
+const DirectorType = new GraphQLObjectType({
+  name: 'Director',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -16,8 +44,17 @@ const Query = new GraphQLObjectType({
   fields: {
     movie: {
       type: MovieType,
-      args: { id: { type: GraphQLString } },
-      resolve(parent, args) {},
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return movies.find(movie => movie.id === args.id);
+      },
+    },
+    director: {
+      type: DirectorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return directors.find(director => director.id === args.id);
+      },
     },
   },
 });
